a0ca6b6bed87f58568d771c16d46567c4aa52bde
ToshBrown <Toshbrown@gmail.com>
2018-01-17 15:48:36 +0000

    Enable component persistance
    For https://github.com/me-box/databox/issues/226

--- src/main.js
+++ src/main.js
@@ -10,11 +10,19 @@ httpsHelper.init()
 		conman.setHttpsHelper(httpsHelper);
 		return conman.connect();
 	})
-	.then(() => {
+	.then(()=>{
+		return conman.reloadInstalledComponents();
+	})
+	.then((slas) => {
 		//require here so env vars are set!
 		containerMangerUIServer = require('./server.js');
 		//set up the arbiter proxy
 		containerMangerUIServer.proxies['arbiter'] = 'arbiter:8080';
+
+		slas.forEach(sla => {
+			//set up the proxy
+			containerMangerUIServer.proxies[sla.name] = sla.name + ':8080';
+		});
 
 		console.log("Starting UI Server!!");
 		return containerMangerUIServer.launch(conman);
